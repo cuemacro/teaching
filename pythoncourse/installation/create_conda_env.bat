08fb54d4b761e4ec625c97b7314983823788028f
saeedamen <saeedamen@hotmail.com>
2019-10-12 17:25:03 +0100

    Fixed typos and added GPU ML libraries

--- pythoncourse/installation/create_conda_env.bat
+++ pythoncourse/installation/create_conda_env.bat
@@ -6,6 +6,7 @@ call conda update -n base -c defaults conda --yes
 REM remove any existing environment called py36, and create a py36 with anaconda packages
 REM call conda remove -n py36 --all
 call conda create -n py36 python=3.6 anaconda --yes
+call conda deactivate
 call conda activate py36
 
 REM newer versions of Pandas aren't supported as well by other libraries (eg. Arctic), so stick to 0.24.2
@@ -14,15 +15,14 @@ call conda install -c anaconda pandas=0.24.2 --yes
 REM install from conda-forge (pyarrow is a newer version!)
 call conda install -c conda-forge ^
 jupyterlab jupyter_contrib_nbextensions jupyter_nbextensions_configurator ^
-redis-py=3.3.7 python-blosc=1.8.1 pathos graphviz python-graphviz rise textblob wordcloud quandl spacy --yes
+redis-py=3.3.7 python-blosc=1.8.1 pathos graphviz python-graphviz rise textblob wordcloud quandl spacy fastparquet --yes
 
 call conda install -c conda-forge pystan fbprophet --yes
 
-REM graphics libraries
-call pip install cufflinks==0.16 plotly_express==0.3.1 dash==1.1.1 dash-html-components==1.0.0 dash-core-components==1.1.1 plotly==3.10.0 dash-table==4.1.0 --yes
-
-REM to be able to plot Plotly into PNG or JPG
-call conda install -c plotly plotly-orca --yes
+REM install PyTorch
+REM only if you have GPU
+REM call conda install pytorch torchvision cudatoolkit=10.0 -c pytorch --yes
+call conda install pytorch torchvision cpuonly -c pytorch --yes
 
 REM Jupyter libraries
 call jupyter contrib nbextension install --user
@@ -31,11 +31,22 @@ call jupyter-nbextension install rise --py --sys-prefix
 
 call pip install arctic==1.79.2
 
+REM install tensorflow and transformers/huggingface
+REM only if you have GPU
+REM call pip install tensorflow-gpu transformers
+call pip install tensorflow transformers
+
 REM for findatapy
-call pip install fxcmpy alpha_vantage yfinance twython newspaper3k seasonal pdfminer.six --yes
+call pip install fxcmpy alpha_vantage yfinance twython newspaper3k seasonal pdfminer.six vaderSentiment --yes
 
 REM findatapy, chartpy and finmarketpy
 REM needs git
-REM pip install git+https://github.com/cuemacro/finmarketpy.git git+https://github.com/cuemacro/chartpy.git git+https://github.com/cuemacro/findatapy.git
+REM call pip install git+https://github.com/cuemacro/finmarketpy.git git+https://github.com/cuemacro/chartpy.git git+https://github.com/cuemacro/findatapy.git
+
+REM graphics libraries
+call pip install cufflinks==0.16 plotly_express==0.3.1 dash==1.1.1 dash-html-components==1.0.0 dash-core-components==1.1.1 plotly==3.10.0 dash-table==4.1.0
+
+REM to be able to plot Plotly into PNG or JPG
+call conda install -c plotly plotly-orca --yes
 
 
--- pythoncourse/installation/create_conda_env.sh
+++ pythoncourse/installation/create_conda_env.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-conda remove -n py36 --all
+# update conda
+conda update -n base -c defaults conda --yes
+
+# remove any existing environment called py36, and create a py36 with anaconda packages
+conda deactivate
+
+# conda remove -n py36 --all
 conda create -n py36 python=3.6 anaconda
 
-source deactivate
-source activate py36
+conda deactivate
+conda activate py36
 
 # newer versions of Pandas aren't supported as well by other libraries (eg. Arctic), so stick to 0.24.2
 conda install -c anaconda pandas=0.24.2 --yes
@@ -12,34 +18,42 @@ conda install -c anaconda pandas=0.24.2 --yes
 # install from conda-forge (pyarrow is a newer version!)
 conda install -c conda-forge \
 jupyterlab jupyter_contrib_nbextensions jupyter_nbextensions_configurator \
-redis-py=3.3.7 python-blosc=1.8.1 pathos pyarrow=0.14.1 graphviz python-graphviz rise textblob wordcloud quandl keras \
+redis-py=3.3.7 python-blosc=1.8.1 pathos pyarrow=0.14.1 graphviz python-graphviz rise textblob wordcloud quandl \
 vaex-core vaex-viz vaex-jupyter vaex-arrow vaex-server vaex-hdf5 vaex-astro vaex-distributed pyspark py4j spacy --yes
 
-# graphics libraries
-pip install cufflinks==0.16 plotly_express==0.3.1 dash==1.1.1 dash-html-components==1.0.0 dash-core-components==1.1.1 plotly==3.10.0 dash-table==4.1.0 --yes
-
 # for database
-pip install arctic=1.79.2 --yes
+pip install arctic==1.79.2
 
-# to be able to plot Plotly into PNG or JPG
-conda install -c plotly plotly-orca --yes
+# install PyTorch
+# only if you have GPU
+# conda install pytorch torchvision cudatoolkit=10.0 -c pytorch --yes
+conda install pytorch torchvision cpuonly -c pytorch --yes
 
 jupyter contrib nbextension install --user # to activate js on Jupyter
 jupyter nbextension enable execute_time/ExecuteTime
 jupyter-nbextension install rise --py --sys-prefix
 
 # celery library
-pip install celery==4.3.0 celery[redis] celery[msgpack] msgpack-python --yes
+pip install celery==4.3.0 celery[redis] celery[msgpack] msgpack-python
 
 # for findatapy
-pip install fxcmpy alpha_vantage yfinance twython newspaper3k seasonal pdfminer.six --yes
+pip install fxcmpy alpha_vantage yfinance twython newspaper3k seasonal pdfminer.six vaderSentiment vaderSentiment gensim textblob
+
+# install tensorflow and transformers/huggingface
+# only if you have GPU
+# pip install tensorflow-gpu transformers
+pip install tensorflow transformers
 
 # modin (downgrade pandas version)
 # may downgrade to pandas to 0.24.2
-pip install modin --yes
+pip install modin
 
 # findatapy, chartpy and finmarketpy (or can manually clone these from GitHub and add to your Python path)
-# sudo yum install git # for Linux
-pip install git+https://github.com/cuemacro/finmarketpy.git git+https://github.com/cuemacro/chartpy.git git+https://github.com/cuemacro/findatapy.git --yes
+pip install git+https://github.com/cuemacro/finmarketpy.git git+https://github.com/cuemacro/chartpy.git git+https://github.com/cuemacro/findatapy.git
 
+# graphics libraries
+pip install cufflinks==0.16 plotly_express==0.3.1 dash==1.1.1 dash-html-components==1.0.0 dash-core-components==1.1.1 plotly==3.10.0 dash-table==4.1.0
+
+# to be able to plot Plotly into PNG or JPG
+conda install -c plotly plotly-orca --yes
 
