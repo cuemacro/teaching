1b079603ccc79dbb93d27b1c0a3393b0c5571e6a
saeedamen <saeedamen@hotmail.com>
2020-10-21 13:07:35 +0100

    Update xlwings env

--- pythoncourse/start_xlwings.bat
+++ pythoncourse/start_xlwings.bat
@@ -1,7 +1,7 @@
 REM illustrates how to start xlwings to force it to include your conda environment
 REM note you may need to change some of the PYTHONPATH or PATH variables (or you can add your own)
 call C:\Anaconda3\Scripts\activate.bat
-call conda activate py36class
+call conda activate py37class
 
 set PYTHONPATH=e:\cuemacro\chartpy\;e:\cuemacro\findatapy\;e:\cuemacro\finmarketpy\;e:\cuemacro\pythoncourse\;%PYTHONPATH%
 set PATH=%PATH%
