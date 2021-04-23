8e45b262b766b3551fcf3b8befb6f6e4ef23deae
saeedamen <saeedamen@hotmail.com>
2021-04-23 14:00:32 +0100

    Updated Mac to Python 3.8

--- pythoncourse/installation/create_conda_env_mac.sh
+++ pythoncourse/installation/create_conda_env_mac.sh
@@ -20,46 +20,45 @@ source activate
 conda install mamba -n base -c conda-forge
 
 # Remove any existing environment called py37class, and create a py37class with anaconda packages
-mamba remove -n py37class --all --yes
-mamba create -n py37class python=3.7
-conda activate py37class
-source activate py37class
+mamba remove -n py38class --all --yes
+mamba create -n py38class python=3.8
+conda activate py38class
+source activate py38class
 
 # Install Tensorflow, PyTorch and Anaconda (lots of packages)
 # only if you have GPU below 2 lines instead of CPU versions (although this hasn't been tested on Mac)
-# mamba install anaconda tensorflow-gpu=2.0.0 pandas=1.0.5 scikit-learn graphviz python-graphviz matplotlib xlwings -c anaconda --yes
-# mamba install pytorch torchvision cudatoolkit=10.1 \
-mamba install anaconda tensorflow=2.0.0 pandas=1.0.5 scikit-learn graphviz python-graphviz matplotlib xlwings -c anaconda --yes
-
-# Install from conda-forge (pyarrow is a newer version!)
-mamba install \
+# mamba install anaconda tensorflow-gpu=2.0.0 pandas=1.2.3 scikit-learn graphviz python-graphviz matplotlib xlwings=0.23.0 \
+#   pytorch torchvision cudatoolkit=10.1 \
+mamba install anaconda tensorflow=2.0.0 pandas=1.2.3 scikit-learn graphviz python-graphviz matplotlib xlwings=0.23.0 \
   pytorch torchvision cpuonly \
-  pyarrow opencv modin=0.8.0 pyemd \
-  boto3 jupyter_contrib_nbextensions jupyter_nbextensions_configurator \
+  pyarrow opencv modin pyemd \
+  boto3 \
   redis-py python-blosc pathos textblob \
-  vaex=3.0.0 \
+  vaex=4.0.0 \
   spacy fastparquet python-snappy nodejs pystan fbprophet setuptools-git \
   pyspark koalas \
   vispy datashader pyproj holoviews streamz quandl bqplot \
-  -c pytorch -c conda-forge \
+  notebook=6.1.4 voila jupyter_contrib_nbextensions jupyter_nbextensions_configurator \
+  -c anaconda -c pytorch -c conda-forge \
   --yes
 
 # Install database
 # Install Celery
-# For findatapy (and NLP) and complex graphics
+# For findatapy and NLP and complex graphics
 # Install transformers/huggingface and table libraries
 # Install graphics/plotting libraries
 # Install findatapy, chartpy and findatapy
-pip install arctic==1.79.2 \
-   celery==4.4.0 celery[redis] celery[msgpack] msgpack-python \
-   fxcmpy alpha_vantage yfinance twython newspaper3k seasonal pdfminer.six vaderSentiment gensim wordcloud RISE requests_html \
-   textacy==0.8.0 \
-   transformers==3.0.2 pytesseract cmdstanpy==0.4 tabula-py==1.4.3 \
+pip install arctic==1.79.4 \
+   celery==5.0.5 celery[redis] celery[msgpack] msgpack-python \
+   fxcmpy alpha_vantage yfinance twython newspaper3k seasonal pdfminer.six vaderSentiment gensim wordcloud rise requests_html \
+   textacy sentencepiece tabula-py \
+   transformers==3.0.2 pytesseract cmdstanpy==0.4  \
    cvlib \
    cufflinks==0.17.3 plotly==4.14.3 kaleido \
-        dash==1.19.0 dash-html-components==1.1.2 dash-core-components==1.15.0 dash-table==4.11.2 jupyter-dash==0.4.0 chart_studio==1.1.0 \
-        dtale==1.8.1 pyldavis progressbar2==3.38.0 \
-   finmarketpy chartpy findatapy financepy
+        dash==1.20.0 dash-html-components==1.1.3 dash-core-components==1.16.0 dash-table==4.11.3 jupyter-dash==0.4.0 chart_studio==1.1.0 \
+        jupyter-book==0.10.2 jupyterbook-latex \
+        dtale==1.43.0 pyldavis progressbar2==3.38.0 \
+   finmarketpy chartpy findatapy financepy findspark pandas==1.2.3 numpy==1.19.1
 
 # GPU libraries for working with DataFrames
 # conda install -c rapidsai -c nvidia -c conda-forge -c defaults rapids=0.10 python=3.7
